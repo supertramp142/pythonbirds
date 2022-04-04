3e2cf900ad8ed95d47ad3abeaa4da9d5ea8e1ede
Supertramp <josuel.mma@gmail.com>
2022-04-04 09:12:31 -0300

    criado o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,11 @@
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return 'Olá'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
+
